5679bcb9021631c20470924f74898cfc79cd929b
hooniegit <neivekim76@gmail.com>
2025-04-05 16:09:57 +0900

    250405 feat - Add MSSQL Service to KafkaConsumer

---
+++ app/01_Receiver/KafkaConsumer/src/main/java/com/hooniegit/KafkaConsumer/MSSQL/Reference.java
@@ -0,0 +1,40 @@
+package com.hooniegit.KafkaConsumer.MSSQL;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+
+/**
+ * 
+ */
+
+@Component
+public class Reference {
+
+    // <id, index> 
+    @Getter
+    private ConcurrentHashMap<Integer, Integer> ids = new ConcurrentHashMap<>();
+
+    // <group, [state, statusOne, statusTwo]>
+    @Getter
+    private ConcurrentHashMap<Integer, Integer[]> groups = new ConcurrentHashMap<>();
+    
+    /**
+     * update ids map
+     * @param ids
+     */
+    public void updateIds(ConcurrentHashMap<Integer, Integer> ids) {
+        this.ids = ids;
+    }
+
+    /**
+     * update groups map
+     * @param groups
+     */
+    public void updateGroups(ConcurrentHashMap<Integer, Integer[]> groups) {
+        this.groups = groups;
+    }
+
+}
